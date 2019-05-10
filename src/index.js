a1c14d2fec9c6968b6c65ad1087278157b4212da
mchrbn <mchrbn@gmail.com>
2019-05-10 16:49:26 +0800

    update landing

--- src/index.js
+++ src/index.js
@@ -3,28 +3,53 @@ import './style.scss';
 const $ = require('jquery');
 window.jQuery = $;
 
-function resize(){
-	// var w = $(window).width();
-	// var h = $(window).height();
+const boards = {
+	SEE : 'see',
+	MOVE : 'move',
+	HEAR : 'hear'
+};
 
-	// var biw = (w * 650) / 1440;
-	// var bih = (biw * 632) / 650;
+var cBoardBg = boards.SEE;
 
-	// $('#board').css({'width' : biw + 'px', 'height' : bih + 'px'});
+function changeBackgroundLanding(){
+	if(cBoardBg == boards.SEE){
+		changeToMove();
+		cBoardBg = boards.MOVE;
+	}
+	else if(cBoardBg == boards.MOVE){
+		changeToHear();
+		cBoardBg = boards.HEAR;
+	}
+	else if(cBoardBg == boards.HEAR){
+		changeToSee();
+		cBoardBg = boards.SEE;
+	}
+}
+
+function changeToSee(){
+	$('#landing').addClass('bg-2-red')
+	.on('animationend', function(){
+        $('#landing').css({'backgroundColor' : '#F38787'});
+		$('#landing').removeClass('bg-2-red');
+	});
+}
+
+function changeToMove(){
+	$('#landing').addClass('bg-2-black')
+	.on('animationend', function(){
+        $('#landing').css({'backgroundColor' : '#3D3D3D'});
+		$('#landing').removeClass('bg-2-black');
+	});
+}
+
+function changeToHear(){
+	$('#landing').addClass('bg-2-yellow')
+	.on('animationend', function(){
+        $('#landing').css({'backgroundColor' : '#F1D8B3'});
+		$('#landing').removeClass('bg-2-yellow');
+	});
 }
 
 $(document).ready(function(){
-	resize();
+	setInterval(changeBackgroundLanding, 5000);
 });
-
-$(window).resize(function(){
-	resize();
-});
-
-// $(document).keypress(function(){
-// 	$('#landing').addClass('bg-2-black')
-// 	.on('animationend', function(){
-// 		$('#landing').css({'backgroundColor' : '#333333'});
-// 		$('#landing').removeClass('bg-2-black');
-// 	});
-// });
